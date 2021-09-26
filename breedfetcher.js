4245c3c11f9df9324ade93091ad69f054d186504
haiming <hesmaeilz01@gmail.com>
2021-09-25 20:38:11 -0400

    fetch a cat by its breed

---
+++ breedfetcher.js
@@ -0,0 +1,30 @@
+const request = require('request');
+const url = `https://api.thecatapi.com/v1/breeds/search?q=`;
+const breedName = process.argv[2];
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `${url}${breedName}`,
+    (error, resp, body) => {
+      const data = JSON.parse(body);
+      // console.log(data);
+      // console.log(typeof body);
+      // console.log(typeof data);
+      const breed = data[0].description;
+      if (body === '[]') {
+        callback('ERROR: invalid breed', null);
+      }
+      callback(null, breed);
+    });
+};
+
+fetchBreedDescription(breedName, (error, description) => {
+  if (error) {
+    console.log('ERROR:', error);
+  } else {
+    console.log(description);
+  }
+});
+
+
+// module.exports = { fetchBreedDescription };
